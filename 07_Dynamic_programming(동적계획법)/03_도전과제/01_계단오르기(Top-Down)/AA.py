555fe745ba26cdff9b97b62d4f4d11935c289bd7
rossa <fashionfish@naver.com>
2021-06-11 23:14:09 +0900

    07 Dynamic programming(동적계획법) 3 ~ 4

---
+++ 07_Dynamic_programming(동적계획법)/03_도전과제/01_계단오르기(Top-Down)/AA.py
@@ -0,0 +1,35 @@
+import sys
+
+sys.stdin = open("input.txt", "r")
+
+"""
+도전과제 : 계단오르기(Top-Down : 메모이제이션)
+철수는 계단을 오를 때 한 번에 한 계단 또는 두 계단씩 올라간다. 만약 총 4계단을 오른다면 
+그 방법의 수는 
+1+1+1+1, 1+1+2, 1+2+1, 2+1+1, 2+2 로 5가지이다. 
+그렇다면 총 N계단일 때 철수가 올라갈 수 있는 방법의 수는 몇 가지인가?
+▣ 입력설명
+첫째 줄은 계단의 개수인 자연수 N(3≤N≤45)이 주어집니다.
+▣ 출력설명
+첫 번째 줄에 올라가는 방법의 수를 출력합니다.
+▣ 입력예제 1 
+7
+▣ 출력예제 1
+21
+"""
+
+
+def DFS(step):
+    if dy[step] > 0:
+        return dy[step]
+    if step == 1 or step == 2:
+        return step
+    else:
+        dy[step] = DFS(step - 1) + DFS(step - 2)
+        return dy[step]
+
+
+if __name__ == "__main__":
+    n = int(input())
+    dy = [0] * (n + 1)
+    print(DFS(n))
